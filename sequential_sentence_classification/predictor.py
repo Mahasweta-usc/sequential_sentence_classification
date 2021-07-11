a53d06d5d479925df6f2bf7a99d2ee55cb50157f
Mahasweta-usc <mahaswec@usc.edu>
2021-07-12 04:09:09 +0530

    generate embeddings

--- sequential_sentence_classification/predictor.py
+++ sequential_sentence_classification/predictor.py
@@ -164,7 +164,7 @@ class SeqClassificationPredictor(Predictor):
 		#comment for only segmentation and prediction
 		# f["last_reply"] = f["content"].apply(lambda x: sent_break(process_(EmailReplyParser.parse_reply(x.replace('.>','\n>')))))
 		# f["IS_count"] = [0]*f.shape[0]
-		# f["IS_"] = [""]*f.shape[0]
+		f["embeddings"] = [""]*f.shape[0]
 
 		json_data = process_text(f)
 		print("Emails processed: ", len(list(json_data.keys())))
@@ -174,22 +174,27 @@ class SeqClassificationPredictor(Predictor):
 			sentences = json_data[url]["sentences"]
 			labels = json_data[url]["labels"]
 			predictions = []#;print(url)
-
+			embeddings = []
 			for sentence, label in zip(sentences,labels):
 				instances = self._dataset_reader.text_to_instance(sentences=sentence,labels=label)
 				output = self._model.cuda().forward_on_instances([instances])
 				# print(sentence)
 				idx = output[0]['action_probs'].argmax(axis=1).tolist()
 				logits = [self._model.vocab.get_token_from_index(i, namespace='labels') for i in idx]
-				embeddings = output[0]['embeddings'];print(type(embeddings))
+				embeddings += output[0]['embeddings'].tolist();print(type(embeddings))
 				binary_labels = [int(item.split("_")[0]) for item in logits]
 				predictions += list(itertools.compress(sentence,binary_labels)) #;print(sum(binary_labels))
 
-			pred_out = list(set(predictions))
+			final_embed = []
 			org_preds = set(row["IS_"].split("<Institutional>"))
-			assert len(pred_out) == len(org_preds)
+
+			for index,pred in predictions:
+				if pred in org_preds: 
+					final_embed.append(embeddings[index])
+					org_preds.remove(pred)
+			assert len(final_embed) == len(org_preds)
 			# print("Predicted:",len(set(row["IS_"].split("<Institutional>"))))# print("IS count: ",len(pred_out));
-			# f.at[indx,"IS_"] = "<Institutional>".join(pred_out)
+			f.at[indx,"embeddings"] = np.array(final_embed)
 			# f.at[indx,"IS_count"] = len(pred_out)
 			if not indx%100: f.to_csv(outfile,index=False);print(indx,f[f["IS_count"] > 0].shape[0])
 			
