0a6feda4dee005fec218e88c45611220ea211177
Mahasweta-usc <mahaswec@usc.edu>
2021-07-12 02:18:30 +0530

    check embedding type

--- sequential_sentence_classification/predictor.py
+++ sequential_sentence_classification/predictor.py
@@ -181,12 +181,14 @@ class SeqClassificationPredictor(Predictor):
 				# print(sentence)
 				idx = output[0]['action_probs'].argmax(axis=1).tolist()
 				logits = [self._model.vocab.get_token_from_index(i, namespace='labels') for i in idx]
-				# embeddings = output[0]['embeddings']#;print(embeddings)
+				embeddings = output[0]['embeddings'];print(type(embeddings))
 				binary_labels = [int(item.split("_")[0]) for item in logits]
 				predictions += list(itertools.compress(sentence,binary_labels)) #;print(sum(binary_labels))
 
-			pred_out = list(set(predictions));print(indx,len(set(predictions).intersection(set(row["IS_"].split("<Institutional>")))))
-			print("Predicted:",len(set(row["IS_"].split("<Institutional>"))))# print("IS count: ",len(pred_out));
+			pred_out = list(set(predictions))
+			org_preds = set(row["IS_"].split("<Institutional>"))
+			assert len(pred_out) == len(org_preds)
+			# print("Predicted:",len(set(row["IS_"].split("<Institutional>"))))# print("IS count: ",len(pred_out));
 			# f.at[indx,"IS_"] = "<Institutional>".join(pred_out)
 			# f.at[indx,"IS_count"] = len(pred_out)
 			if not indx%100: f.to_csv(outfile,index=False);print(indx,f[f["IS_count"] > 0].shape[0])
