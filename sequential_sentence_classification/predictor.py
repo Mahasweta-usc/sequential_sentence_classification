acaaaac28d9c3667e1b842286d227ae356bb7c77
Mahasweta-usc <mahaswec@usc.edu>
2022-02-24 13:57:37 -0800

    fse commits for predictor

--- sequential_sentence_classification/predictor.py
+++ sequential_sentence_classification/predictor.py
@@ -1,228 +1,215 @@
-# from typing import List
-# from overrides import overrides
-# from allennlp.common.util import JsonDict, sanitize
-# from allennlp.data import Instance
-# from allennlp.predictors.predictor import Predictor
-# import argparse
-# import jsonlines
-# import os, sys
-# os.environ["CUDA_VISIBLE_DEVICES"]= '0'
-# os.environ["cuda_device"]= '0'
-# import random
-# import json
-# import numpy as np
-# import regex as re
-# from fuzzywuzzy import fuzz, process
-# import itertools
-# from itertools import combinations, compress
-# import nltk
-# nltk.download('punkt')
-# from nltk.tokenize import sent_tokenize,word_tokenize
-# import pandas as pd
-# import stanza
-# stanza.download('en')
-# nlp = stanza.Pipeline(lang='en', processors='tokenize',use_gpu=True,tokenize_batch_size=4)
-# from email_reply_parser import EmailReplyParser
-# from transformers import BertTokenizer
-# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
-# MAX_LEN = 256
-# from tqdm import tqdm
-# import multiprocessing
-# import torch
-# from ast import literal_eval
-# from multiprocessing import Pool, Manager #, set_start_method
-# # try:
-# # 	set_start_method('spawn', force=True)
-# # except RuntimeError:
-# #     pass
+
+#For full email prediction
+
+from typing import List
+from overrides import overrides
+from allennlp.common.util import JsonDict, sanitize
+from allennlp.data import Instance
+from allennlp.predictors.predictor import Predictor
+import argparse
+import jsonlines
+import os, sys
+os.environ["CUDA_VISIBLE_DEVICES"]= '0'
+os.environ["cuda_device"]= '0'
+import random
+import json
+import numpy as np
+np.random.seed(0) 
+import regex as re
+from fuzzywuzzy import fuzz, process
+import itertools
+from itertools import combinations, compress
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize,word_tokenize
+import pandas as pd
+import stanza
+stanza.download('en')
+nlp = stanza.Pipeline(lang='en', processors='tokenize',use_gpu=True,tokenize_batch_size=4)
+from email_reply_parser import EmailReplyParser
+from transformers import BertTokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+MAX_LEN = 256
+from tqdm import tqdm
+import multiprocessing
+import torch
+from ast import literal_eval
+from multiprocessing import Pool, Manager #, set_start_method
+# try:
+# 	set_start_method('spawn', force=True)
+# except RuntimeError:
+#     pass
     
 
 
-# # mn = Manager()
-# candidate = dict()
-# email_sent = dict()
-	
+mn = Manager()
+candidate = mn.dict()
+email_sent = mn.dict()
 
+def process_(text):
+  text = text.replace("\r\n"," ").replace("\n"," ")
+  text = " ".join(re.sub('>',"",text, flags=re.IGNORECASE).split())
+  return text
 
+def sent_break(text):
+	doc = nlp(text)
+	lines = [line.text for line in doc.sentences if len(line.text) > 1]
+	return lines
 
-# def process_(text):
-#   text = text.replace("\r\n"," ").replace("\n"," ")
-#   text = " ".join(re.sub('>',"",text, flags=re.IGNORECASE).split())
-#   return text
+def chunks(lst, n):
+	for i in range(0, len(lst), n):
+		yield lst[i:i + n]
 
-# def sent_break(text):
-# 	doc = nlp(text)
-# 	lines = [line.text for line in doc.sentences if len(line.text) > 1]
-# 	return lines
+def single_entries(url):
+  global candidate
+  for idx, elem in enumerate(candidate[url]):
+    if len(elem) < 2:
+      elem = tokenizer.tokenize(elem[0])
+      length = MAX_LEN - len(elem) - 1
+      if MAX_LEN > 2*len(elem):
+        addage = tokenizer.convert_tokens_to_string(elem)
+        candidate[url][idx].append(addage)
+      else:
+        candidate[url][idx] = []
+        for subsent in chunks(elem,int(len(elem)/2) + 1):
+          addage = tokenizer.convert_tokens_to_string(subsent)
+          candidate[url][idx].append(addage)
 
+def decode(seg):
+	# chunk = tokenizer.convert_ids_to_tokens(seg)
+	text = tokenizer.convert_tokens_to_string(seg)
+	return text 
 
-# def prune(url):
-# 	global candidate
-# 	temp = []
-# 	for elem in candidate[url]:
-# 		if any(len(elem) < len(cand) and set(elem).issubset(set(cand)) for cand in candidate[url]):
-# 			temp.append(elem)
-# 	for elem in temp: candidate[url].remove(elem)
+def segmenter(url):
+  global candidate;email_sent
+  sum_ = 0;
+  lim = int(0.9*MAX_LEN)
+  # print("before",candidate[url])
+  seg = []
+  for idx, elem in enumerate(email_sent[url]):
+    remain = lim - sum_
+    # print("after",candidate[url])
+    sum_ += len(tokenizer.encode(elem))
+    if sum_ > lim:
+      retain = tokenizer.convert_tokens_to_string(tokenizer.tokenize(elem[:remain]))
+      carryover = tokenizer.convert_tokens_to_string(tokenizer.tokenize(elem[remain:]))
+      if not idx:
+        email_sent[url][idx] = carryover
+        email_sent[url].insert(idx,retain)
+        seg.append(retain)
+        # return idx + 1
+      return seg
+    
+    seg.append(elem)
+  return seg
 
-# def single_entries(url):
-# 	global candidate
-# 	for idx, elem in enumerate(candidate[url]):
-# 		if len(elem) == 1:
-# 			if MAX_LEN > 2*len(tokenizer.tokenize(elem[0])):
-# 				candidate[url][idx].append(elem[0])
-# 			else:
-# 				str_len = int(len(elem[0])/2)
-# 				candidate[url][idx] = [elem[0][:str_len]]
-# 				candidate[url][idx].append(elem[0][str_len:])
+def email_to_json(url):
+	global candidate
+	json_data = []
+	entry = {url:{}}
+	entry[url]["sentences"] = candidate[url]
+	entry[url]["labels"] = [["0"]*len(row) for row in candidate[url]]
+	entry[url]["abstract_id"] = 0
+	assert len(entry[url]["labels"]) == len(entry[url]["sentences"])
+	return entry
 
-# def segmenter(url):
-# 	global candidate, email_sent
-# 	sum_ = 0;
-# 	lim = int(0.9*MAX_LEN)
-# 	candidate[url].append([])
-# 	for idx, elem in enumerate(email_sent[url]):
-# 		remain = lim - sum_
-# 		sum_ += len(tokenizer.encode(elem))
-# 		if sum_ > lim:
-# 			retain = tokenizer.convert_tokens_to_string(tokenizer.tokenize(elem[:remain]))
-# 			carryover = tokenizer.convert_tokens_to_string(tokenizer.tokenize(elem[remain:]))
-# 			if not idx:
-# 				email_sent[url][idx] = carryover
-# 				email_sent[url].insert(idx,retain)
-# 				candidate[url][-1].append(retain) 
-# 				# return idx + 1
-# 			return idx
-# 		candidate[url][-1].append(elem)
+def prune(url):
+	global candidate
+	temp = []
+	for elem in candidate[url]:
+		if any(len(elem) < len(cand) and set(elem).issubset(set(cand)) for cand in candidate[url]):
+			temp.append(elem)
+	for elem in temp: candidate[url].remove(elem)
 
+def segment_text(chunk,url,current):
+	global email_sent, candidate
+	email_sent[url] = chunk["last_reply"][:50]
+	candidate[url] = []
+	while True:
+		last_seg = segmenter(url)
+		if last_seg: candidate[url] += [last_seg] #;print(len(candidate[url][-1]),len(email_sent[url]))
+		win_ = int(0.5*len(last_seg))
+		# if len(email_sent[url]) > 1: email_sent[url].pop(0)
+		# else: break
+		if win_ < len(email_sent[url]) :email_sent[url]=email_sent[url][win_+1:]
+		else:break
+	prune(url)
+	single_entries(url)
+	json_results = email_to_json(url)
+	return json_results
 
-# def email_to_json(url):
-# 	global candidate
-# 	json_data = []
-# 	entry = {url:dict()}
-# 	entry[url]["sentences"] = candidate[url]
-# 	entry[url]["labels"] = [["0"]*len(row) for row in candidate[url]]
-# 	entry[url]["abstract_id"] = 0
-# 	assert len(entry[url]["labels"]) == len(entry[url]["sentences"])
-# 	return entry
+def segment_email(x):
+	try:
+		return sent_break(process_(EmailReplyParser.parse_reply(x.replace('.>','\n>'))))
+	except:
+		return sent_break(process_(x.replace('.>','\n>')))
 
+def process_text(f):
+	n_cpu = int(multiprocessing.cpu_count()/3) # //2 #1 # multiprocessing.cpu_count() //2
+	pool = Pool(n_cpu)
+	results = []
+	print("Segmenting emails")
+	for i ,row in f.iterrows():
+		# results.append(segment_text(row, row["message_id"], i))
+		results.append(pool.apply_async(segment_text, args=(row,row["message_id"],i)))
 
-# def segment_text(chunk,url,current):
-# 	global candidate, email_sent
-# 	email_sent[url] = chunk["last_reply"]
-# 	candidate[url] = []
-# 	while True:
-# 		pos = segmenter(url)
-# 		win = min(len(email_sent[url]),int(len(candidate[url][-1])/2) + 1)
-# 		if win > 0 and len(candidate[url]) < 25:
-# 			for _ in range(win): email_sent[url].pop(0)
-# 		else: break
-
-# 	candidate[url] = [cand for cand in candidate[url] if cand]
-# 	prune(url)
-# 	single_entries(url)
-# 	# if len(candidate[url]) > 20 or not len(candidate[url]): candidate[url] = candidate[url][:20]
-# 	# print(len(candidate[url]))
-# 	json_results = email_to_json(url)
-# 	if not current%100: print("{} emails segmented".format(current))
-# 	return json_results
-
-
-# def process_text(f):
-# 	n_cpu = 2 # multiprocessing.cpu_count() //2 #1 # multiprocessing.cpu_count() //2
-
-# 	pool = Pool(n_cpu)
-# 	results = []
-# 	print("Segmenting emails")
-# 	for i ,row in f.iterrows():
-# 		# results.append(segment_text(row, row["message_id"], i))
-# 		results.append(pool.apply_async(segment_text, args=(row,row["message_id"],i)))
-
-# 	pool.close()
-# 	pool.join()
-# 	out = dict()
-# 	for r in results:
-# 		out.update(r.get())
-# 	return out
+	pool.close()
+	pool.join()
+	out = dict()
+	for r in results:
+		out.update(r.get())
+	return out
  
  
-# @Predictor.register('SeqClassificationPredictor')
-# class SeqClassificationPredictor(Predictor):
-# 	"""
-# 	Predictor for the abstruct model
-# 	""" 
-# 	def predict_json(self, json_dict: JsonDict) -> JsonDict:
-# 		print("Enter full file path: ")
-# 		filename = os.environ["FILE_PREDS"];print(filename)
-# 		#outfile = filename ##for only segmentation and prediction
-# 		outfile = filename.replace(".csv","_IS_graduated.json")
-# 		final_res = dict()
-# 		for month in range(24): final_res[month] = [] 
+@Predictor.register('SeqClassificationPredictor')
+class SeqClassificationPredictor(Predictor):
+	"""
+	Predictor for the abstruct model
+	""" 
+	def predict_json(self, json_dict: JsonDict) -> JsonDict:
+		print("Enter full file path: ")
+		filename = os.environ["FILE_PREDS"];print(filename)
+		f = pd.read_csv(filename)
+		print("No of entires: ",f.shape[0])
+		print("Reading file")
+		#comment for only segmentation and prediction
+		try: 
+			_ = f['last_reply']
+			f['last_reply'] = f.last_reply.apply(lambda x: literal_eval(str(x)))
+		except:
+			f["last_reply"] = f["body"].apply(lambda x: segment_email(x))
 
-# 		f = pd.read_csv(filename,lineterminator='\n');f.dropna(subset=["content","message_id"],inplace=True)
-# 		f = f[(f["status"] == 'graduated') & (f['month'] < 24)][:1000]
-# 		f = f[f["folder"].isin(["dev","user","users","announce"])]
-# 		print("No of entires: ",f.shape[0])
-# 		row_count = 0
-# 		print("Reading file")
-# 		f['last_reply'] = f.last_reply.apply(lambda x: literal_eval(str(x)))
-# 		#comment for only segmentation and prediction
-# 		# f["last_reply"] = f["content"].apply(lambda x: sent_break(process_(EmailReplyParser.parse_reply(x.replace('.>','\n>')))))
-# 		# f["IS_count"] = [0]*f.shape[0]
-# 		f["embeddings"] = [""]*f.shape[0]
+		f["IS"] = [""]*f.shape[0]
+		print("Processing emails")
 
-# 		json_data = process_text(f);miss_count = [0,0,0]
-# 		print("Emails processed: ", len(list(json_data.keys())))
-# 		print("Segmentation done. Starting predictions")
-# 		for indx, row in tqdm(f.iterrows()):
-# 			url = row["message_id"]
-# 			sentences = json_data[url]["sentences"]
-# 			labels = json_data[url]["labels"]
-# 			predictions = []#;print(url)
-# 			embeddings = []
-# 			final_embed = []
+		json_data = process_text(f)
+		print("Emails processed: ", len(list(json_data.keys())),len(list(candidate.keys())),len(list(email_sent.keys())))
+		print("Segmentation done. Starting predictions")
+		for indx, row in tqdm(f.iterrows()):
+			url = row["message_id"]
+			sentences = json_data[url]["sentences"]
+			labels = json_data[url]["labels"]
+			predictions = []
+
+			for sentence, label in zip(sentences,labels):
+				try:
+					self._dataset_reader.predict = True
+					instances = self._dataset_reader.text_to_instance(sentences=sentence)
+					output = self._model.cuda().forward_on_instances([instances])
+					# print(sentence)
+					idx = output[0]['action_probs'].argmax(axis=1).tolist()
+					logits = [self._model.vocab.get_token_from_index(i, namespace='labels') for i in idx]
+					binary_labels = [int(item.split("_")[0]) for item in logits]
+					predictions.extend(list(itertools.compress(sentence,binary_labels))); #print(np.shape(embeddings))
+				except Exception as e: pass
 			
-# 			for sentence, label in zip(sentences,labels):
-# 				instances = self._dataset_reader.text_to_instance(sentences=sentence,labels=label)
-# 				output = self._model.cuda().forward_on_instances([instances])
-# 				# print(sentence)
-# 				idx = output[0]['action_probs'].argmax(axis=1).tolist()
-# 				logits = [self._model.vocab.get_token_from_index(i, namespace='labels') for i in idx]
-# 				binary_labels = [int(item.split("_")[0]) for item in logits]
-# 				embeddings.extend(list(itertools.compress(output[0]['embeddings'].tolist(),binary_labels))); #print(np.shape(embeddings))
-# 				predictions.extend(list(itertools.compress(sentence,binary_labels))) #;print(sum(binary_labels))
-
-			
-# 			assert len(embeddings) == len(predictions)
-# 			org_preds = row["IS_"].split("<Institutional>")
-# 			pred_out = list(set(predictions))
-# 			miss_count[2] += len(org_preds)
-# 			if len(org_preds) != len(pred_out): miss_count[1] += 1;print(org_preds,'\n',pred_out)
-
-# 			for index,pred in enumerate(predictions):
-# 				for x in org_preds: 
-# 					if fuzz.ratio(x,pred) > 90: 
-# 						final_embed.append(embeddings[index])
-# 						org_preds.remove(x)
-
-# 			assert len(final_embed) == len(row["IS_"].split("<Institutional>")) - len(org_preds)
-# 			if len(org_preds): miss_count[0] += 1
-# 			final_res[row['month']] += final_embed
-# 			# print(len(row["IS_"].split("<Institutional>")),len(embeddings))
-# 			# print("Predicted:",len(set(row["IS_"].split("<Institutional>"))))# print("IS count: ",len(pred_out));
-# 			# f.at[indx,"embeddings"] = final_embed
-# 			# f.at[indx,"IS_count"] = len(pred_out)
-# 			if not indx%10000: 
-# 				# with open(outfile, 'w') as fout: json.dump(final_res, fout, indent=4)
-# 				print(indx,len(final_res[row['month']]))
-			
-# 		with open(outfile, 'w') as fout: json.dump(final_res, fout, indent=4)
-# 		print(miss_count)
-# 		exit()
+			##store IS to csv
+			if predictions: f.at[indx,'IS'] = "<IS>".join(set(predictions))
+		f.to_csv(filename.replace(".csv","_IS.csv"))
+		exit()
 
 
-
-
-
+#For test/validation
 from typing import List
 from overrides import overrides
 
@@ -236,31 +223,32 @@ file_path = os.environ["file_path"]
 
 @Predictor.register('SeqClassificationPredictor')
 class SeqClassificationPredictor(Predictor):
-    """
-    Predictor for the abstruct model
-    """
-    def predict_json(self, json_dict: JsonDict) -> JsonDict:
-        pred_labels = []
-        sentences = json_dict['sentences']
-        paper_id = json_dict['abstract_id']
-        try:
-          labels = json_dict['labels']
-        except:
-          labels = [["1"]*len(sent) for sent in sentences]
-        print(sentences,labels)
-        instance = self._dataset_reader.text_to_instance(sentences=sentences,labels=labels)
-        output = self._model.cuda().forward_on_instances([instance])
-        # print(output)
-        idx = output[0]['action_probs'].argmax(axis=1).tolist()
-        # print(idx)
-        labels = [self._model.vocab.get_token_from_index(i, namespace='labels') for i in idx]
-        # print(labels)
-        pred_labels.extend(labels)
-        assert len(pred_labels) == len(sentences)
-        preds = list(zip(sentences, pred_labels))
+  """
+  Predictor for the abstruct model
+  """
+  def predict_json(self, json_dict: JsonDict) -> JsonDict:
+      pred_labels = []
+      sentences = json_dict['sentences']
+      paper_id = json_dict['abstract_id']
+      try:
+        labels = json_dict['labels']
+      except:
+        labels = [["1"]*len(sent) for sent in sentences]
+      print(sentences,labels)
+      self._dataset_reader.predict= True
+      instance = self._dataset_reader.text_to_instance(sentences=sentences)
+      output = self._model.cuda().forward_on_instances([instance])
+      # print(output)
+      idx = output[0]['action_probs'].argmax(axis=1).tolist()
+      # print(idx)
+      labels = [self._model.vocab.get_token_from_index(i, namespace='labels') for i in idx]
+      # print(labels)
+      pred_labels.extend(labels)
+      assert len(pred_labels) == len(sentences)
+      preds = list(zip(sentences, pred_labels))
 
 
-        with jsonlines.open(file_path, mode='a') as writer:
-          json_dict["predictions"] = pred_labels
-          writer.write(json_dict)
-        return paper_id, preds
+      with jsonlines.open(file_path, mode='a') as writer:
+        json_dict["predictions"] = pred_labels
+        writer.write(json_dict)
+      return paper_id, preds
